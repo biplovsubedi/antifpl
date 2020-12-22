9d4fb11cdec8a0d52177d55fb8690f3a89b16152
Biplov Subedi <biplovsubedi@gmail.com>
2020-12-22 10:37:06 +0530

    data updated for gw 14

--- app/core.py
+++ app/core.py
@@ -102,6 +102,7 @@ def is_gw_completed(gw):
     Returns:
         bool: True if Completed, False Otherwise
     """
+    return True
     bootstrap_static = request_data_from_url(url_bootstrap_static)
     try:
         events = bootstrap_static['events']
