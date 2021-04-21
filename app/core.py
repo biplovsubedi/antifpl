c23f10cf4955fc3521d35b5078b8d82512b4f99f
Biplov Subedi <biplovsubedi@gmail.com>
2021-04-21 14:36:56 +0545

    32 start

--- app/core.py
+++ app/core.py
@@ -75,7 +75,7 @@ def find_current_gw():
     Returns:
         int: Gamweeek corresponding to the request time, 0 if invalid
     """
-    return 31
+    return 32
     # with open(fixture_date_file, 'r') as file:
     #     fixtures = file.read()
     # fixture_d = json.loads(fixtures)
@@ -101,7 +101,7 @@ def is_gw_completed(gw):
     Returns:
         bool: True if Completed, False Otherwise
     """
-    return True
+    return False
     bootstrap_static = request_data_from_url(url_bootstrap_static)
     try:
         events = bootstrap_static['events']
